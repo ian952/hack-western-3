0cd25d76bc51f83fa23644a1ac70ce767fae0ee4
William Lo <will-i-am_95@hotmail.com>
2016-10-15 14:14:40 -0400

    fixed errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import '../stylesheets/App.css';
-<<<<<<< HEAD
-import cx from 'classnames';
-const Title = React.createClass({
-  render() {
-    return (
-      <div className={cx('window-size', 'clearfix')}>
-        <div className='container'>
-          <h1 className='header'>
-          	Choose Chews
-          </h1>
-            <div style={{ margin: '300 0 0 0'}}>
-              <div className='row'>
-                <div className='col-md-2 col-md-offset-4 col-sm-2 col-sm-offset-4'>
-       				   <button className={cx('btn','btn-lg', 'btn-brown')}> 
-       					    Host 
-       				   </button>
-                </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-2 col-md-offset-4 col-sm-2 col-sm-offset-4'>
-       				   <button className={cx('btn', 'btn-lg', 'btn-grey')}> 
-                      Join 
-       				   </button>
-                </div>
-              </div>
-            </div>
-        </div>
-=======
+import cx from 'classnames';      
 import FormView from './Form';
 import io from 'socket.io-client';
 import { ModalContainer, ModalDialog } from 'react-modal-dialog';
@@ -65,11 +38,7 @@ class Title extends React.Component {
   render() {
     return (
       <div>
-        <h1 className='header'>Choose Chews</h1>
-        <div className='row'>
-        	<FormView />
-        </div>
-        {
+      	{
 	      	this.state.isShowingModal &&
 	        <ModalContainer onClose={this.handleClose}>
 	          <ModalDialog onClose={this.handleClose}>
@@ -79,10 +48,33 @@ class Title extends React.Component {
 	          </ModalDialog>
 	        </ModalContainer>
       	}
->>>>>>> origin/master
-      </div>
+        <div className='container'>
+        	<div className={cx('window-size', 'clearfix')}>
+	          <h1 className='header'>
+	          	Choose Chews
+	          </h1>
+	            <div style={{ margin: '300 0 0 0'}}>
+	              <div className='row'>
+	                <div className='col-md-3 col-md-offset-4 col-sm-2 col-sm-offset-4'>
+	       				   <button className={cx('roombtn','btn btn-lg', 'btn-brown')}>
+
+	       					    Host 
+	       				   </button>
+	                </div>
+	              </div>
+	              <div className='row'>
+	                <div className='col-md-3 col-md-offset-4 col-sm-2 col-sm-offset-4'>
+	       				   <button className={cx('roombtn', 'btn btn-lg', 'btn-grey')}> 
+	                      Join 
+	       				   </button>
+	                </div>
+	              </div>
+	            </div>
+		        </div>
+		      </div>
+		    </div>
     );
-  }
+  };
 };
 
 function mapStateToProps (state) {
