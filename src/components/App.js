9764f5cfa175084113336107878a7c73a2d866a0
alan-yilun-li <alan.yilun.li@gmail.com>
2016-10-15 03:18:21 -0400

    title screen
    imported new buttons
    (finally spaced them out)
    changed background colour and title text colour

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../stylesheets/App.css';
-
+import '../stylesheets/push-bootstrap-button-pack-master/assets/css/buttons.css';
+import cx from 'classnames';
 const Title = React.createClass({
   render() {
     return (
-      <div>
-        <h1 className='header'>Welcome to React</h1>
+      <div className='container' style={{ background: '#343d46'}}>
+        <h1 className='header' style={{ color: 'white'}}>
+        	Choose Chews
+        </h1>
+      	<div className='center-block btn-group-vertical row'>
+ 				<button className='btn btn-lg btn-primary raised round' style={{ margin: '30 0 0 0'}}> 
+ 					Host 
+ 				</button>
+ 				<button className='btn btn-lg btn-primary raised round' style={{margin: '20 0 0 0'}}> 
+ 					Join 
+ 				</button>
+ 		</div>
       </div>
     );
   }
