e979309d12555e0d7641eefd108cb17a9762d606
William Lo <will-i-am_95@hotmail.com>
2016-10-15 19:14:04 -0400

    socket callback works

--- backend/app.js
+++ backend/app.js
@@ -25,29 +25,30 @@ yelpService.search ('University of Western Ontario', '40000').then((data) => {
 io.on ('connection', function(socket){
 	console.log ('connected');
 
-	socket.on ('newuser', function(data, fn){
+	socket.on ('newuser', function(data, returnData){
 		console.log (data.name + ' connected');
 		databaseService.createPerson(data.name).then((id) => {
 			console.log ('ID: ' + id);
-			fn(id);
+			returnData(id);
 		});
 	});
 
-	socket.on('create', function(data, fn){
+	socket.on('create', function(data, returnData){
 		console.log ('Create room');
 		databaseService.createGroup(data.person_ID).then((groupId) => {
 			console.log('groupID: ' + groupId);
 			socket.join(groupId);
-			fn(groupId);
+			returnData(groupId);
 		})
 	});
 
-	socket.on ('join', function(data, fn){
+	socket.on ('join', function(data, returnData){
 		console.log ('Join room');
 		databaseService.joinGroup(data.person_ID, data.group_ID).then (() => {
 			databaseService.getPersonsInGroup(data.group_ID).then((person_list) => {
 				socket.join(data.group_ID);
 				socket.broadcast.to(data.group_ID).emit('person_list', person_list);
+				returnData();
 			});
 		});
 	});
